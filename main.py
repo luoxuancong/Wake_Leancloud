ee6b3583a4220a15969ed4af61a9e8de67b45715
imgyh <47165525+imgyh@users.noreply.github.com>
2020-06-15 10:46:00 +0800

    增加HTTP状态码判断

--- main.py
+++ main.py
@@ -7,10 +7,10 @@ def get_status(url):
     return r.status_code
 
 def main():
-    status=get_status("https://valine.0941314.xyz/")
+    status=get_status("https://valine.0941314.xyz/") #修改为自己的valine评论地址
 
     if status==200:
-        print("激活成功！")
+        print("激活成功！HTTP状态码为：",status)
     else:
         print("激活失败！HTTP状态码为：",status)
 
