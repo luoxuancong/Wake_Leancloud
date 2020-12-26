888a4294f57236b5d056e27b91acddef260d88e3
罗玄聪 <33108362+luoxuancong@users.noreply.github.com>
2020-12-26 14:05:16 +0800

    Create main.py

---
+++ .github/workflows/main.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_status(url):
+    
+    r=requests.get(url=url)
+
+    return r.status_code
+
+def main():
+    status=get_status("https://blog.lxc666.cn/") #修改为自己的valine评论地址
+
+    if status==200:
+        print("激活成功！HTTP状态码为：",status)
+    else:
+        print("激活失败！HTTP状态码为：",status)
+
+if __name__ == "__main__":
+    main()
